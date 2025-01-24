c50c2c2421a40dc37901842dd3f838ee7be855f2
Liberty <89135267702@mail.ru>
2025-01-25 00:23:00 +0700

    #	modified:   JS/myscripts.js #	modified:   index.html

--- JS/myscripts.js
+++ JS/myscripts.js
@@ -120,7 +120,7 @@ switch (monthNumber) {
 /* Домашка циклы */
 /* Задание 1 */
 
-
+/*
 for (let i = 0; i < 2; i++) {
 
     console.log(`Привет`)
@@ -200,3 +200,370 @@ const Month = { // Обьявляем объект  Month
 for (const key in Month) {    // из ключа в обьекте
     console.log(`${key}:это ${Month[key]} месяц по порядку`); // выводим  ключ : свойство +  текст
 }
+*/
+
+
+// Домашка функции
+//Игра угадай число
+
+function game1(){
+ let userName = prompt(`Введите своё имя`);
+let randomNumber = Math.floor(Math.random()*100)+1;
+
+console.log(randomNumber);
+let guessed =false;
+
+
+while (!guessed) {
+    let userNumber = prompt(`Введите  число от 1 до 100`);
+    
+    if (userNumber>randomNumber) {
+        alert(`Ваше число больше, попробуйте ещё раз`);        
+    }
+        else if(userNumber<randomNumber){
+            alert(`Ваше число меньше, попробуйте ещё раз`)
+        }
+        else {
+            alert(`Вы угадали число `)
+            guessed = true;
+        }
+} 
+}
+
+//Задание 2 Напишите функцию, которая возвращает меньшее из двух чисел.
+
+/* Например, из двух чисел — 
+8 и 4
+ — функция должна вернуть значение 4
+. Если мы передаем два одинаковых значения, например 6 и 6
+, то функция должна вернуть это значение (в данном случае — 6). */
+
+let firstNumber = Math.floor(Math.random() * 1000); //Создаём 1 рандомное число от 1 до 1000
+let secondNumber = Math.floor(Math.random() * 1000); // создаём 2 рандомное число от 1 до 1000
+console.log(`1 число = ${firstNumber}`)
+console.log(`2 число = ${secondNumber}`)
+let comparison = () => {
+
+    console.log(`Сгенерированы случайные числа от 1 до 1000 а именно: ${firstNumber} и ${secondNumber}`)
+    if (firstNumber > secondNumber) {
+        console.log(`В данном случае ${firstNumber} больше чем ${secondNumber} `)
+    }
+    else if (firstNumber < secondNumber) {
+        console.log(`В данном случае ${secondNumber} больше чем ${firstNumber} `)
+
+    }
+    else {
+
+        console.log(`Числа равны и  их значение ${secondNumber}`)
+    }
+}
+comparison();
+
+
+/* Задание 2
+Напишите функцию, которая принимает число и возвращает:
+строку 'Число четное', если число четное;
+строку 'Число нечетное' , если число нечетное.
+Подсказка: четность проверяется с помощью условия 
+n % 2 == 0
+.
+ */
+
+let hiddenNumber = Math.floor(Math.random() * 10000);  // promt // ----->>>
+console.log(`Загаданное число ${hiddenNumber}`)
+hiddenguess(hiddenNumber);
+
+function hiddenguess(hiddenNumber) {
+
+
+    if (hiddenNumber % 2 == 0) {
+        console.log(`Число чётное`);
+    }
+    else {
+        console.log(`Число нечётное`);
+
+    }
+
+
+}
+
+/* Задание 3
+Напишите функцию, которая принимает параметром число и выводит в консоль квадрат этого числа.
+Напишите функцию, которая принимает параметром число и возвращает квадрат этого числа значением — так, чтобы потом это значение можно было использовать.
+
+ */
+let a = Math.floor(Math.random() * 10000);
+let b = 2;
+double(a, b);
+function double(a, b) {
+
+    let mult = a * b;
+    console.log(`Квадрат числа ${a} = ${mult}`);
+    return mult;
+
+}
+
+
+/* Задание 4
+Создайте функцию, которая:
+Спрашивает у пользователя, сколько ему лет. Если пользователь введет отрицательное число — выведет на экран 
+'Вы ввели неправильное значение'.Если пользователь введет число от 
+0
+ до 
+12
+ — выведет на экран 
+'Привет, друг!'
+.
+Если пользователь введет число больше или равно 
+13
+ — выведет на экран 
+'Добро пожаловать!'
+.
+Подсказка: можете выполнить это задание с помощью оператора 
+if else
+. */
+
+
+/* let ageCheck = (userAge = prompt(`Сколько тебе лет`)) => {
+   
+    if (userAge < 0  ) {
+        alert (`Вы  ввели неверное значение ${userAge}`)
+        
+   
+    } 
+    else if(userAge>=13){
+        alert (`Ваш возраст - ${userAge} Добро пожаловать!`)
+    }
+    else {
+        alert (`Привет друг, ты почти взрослый  твой возраст- ${userAge}`)
+        
+    }
+
+}
+ageCheck();
+ */
+
+/*
+Задание 5
+Напишите функцию, которая принимает на вход два числа, а далее следует алгоритму:
+Проверяет, являются ли переданные параметры корректными числами.
+Подсказка: используйте преобразование типов и 
+isNaN()
+.
+
+Если нет — возвращает строку 
+'Одно или оба значения не являются числом'
+.
+Если оба параметра — числа, то возвращает произведение данных чисел. */
+
+let CorrectNumber = (a = prompt(Number(`Введите первое значение`)), b = prompt(Number(`Введите второе значение`))) => {
+    if (isNaN(a) || isNaN(b)) {
+        console.log(`Одно или оба значения не являются числом`);
+
+
+    }
+    else {
+        let mult = a * b;
+        return console.log(`${mult}`);
+
+    }
+
+}
+CorrectNumber();
+
+
+
+/* Задание 6
+Напишите функцию, которая выполняет следующий алгоритм:
+Запрашивает у пользователя число.
+Проверяет, является ли введенное значение числом.
+Если значение не является числом, возвращает строку 
+'Переданный параметр не является числом'.
+Если значение является числом, возвращает строку 'n в кубе равняется <получившееся значение>', где n — введенное число, а <получившееся значение>
+ — число, возведенное в куб.
+Проверьте работу функции с числами от  0 до 10.
+
+ */
+
+
+/* let NumberCheck = (userDifferentNumber = prompt(`Введите любое число`)) => {
+    if(isNaN(userDifferentNumber)) {
+        console.log(`Значение ${userDifferentNumber} - не является числом`);  
+    }
+else{
+    let multi = userDifferentNumber**3;
+    return  console.log(`n в  кубе равно ${multi}`);
+    
+}
+}
+NumberCheck()
+ */
+
+
+
+/* Задание 7
+
+Создайте два объекта, circle1 и circle2, каждый из которых имеет свойство 
+radius.
+
+Оба объекта должны иметь:Метод getArea, который возвращает площадь круга, вычисляемую через радиус.
+Метод 
+getPerimeter, который возвращает периметр окружности. */
+// C= 2p*radius
+//S = p*radius*radius
+/* this.radius*this.radius*p; */
+
+getArea()
+getPerimeter()
+function getArea() {
+    return Math.PI * this.radius ** 2;
+}
+
+function getPerimeter() {
+    return 2 * Math.PI * this.radius;
+}
+
+const circle1 = {
+    radius: 20,
+    methgetArea: getArea,
+    merhgetPerimetr: getPerimeter,
+}
+
+const circle2 = {
+    radius: 10,
+    methgetArea: getArea,
+    merhgetPerimetr: getPerimeter,
+}
+console.log(circle1.merhgetPerimetr);
+console.log(circle1.methgetArea);
+console.log(circle2.merhgetPerimetr);
+console.log(circle2.methgetArea);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задания на воркшопе //
+
+
+
+
+/* Напишите функцию, которая выполняет следующий алгоритм:
+
+Запрашивает у пользователя число.
+Проверяет, является ли введенное значение числом.
+Если значение не является числом, возвращает строку
+'Переданный параметр не является числом'
+.
+Если значение является числом, возвращает строку
+'n в кубе равняется <получившееся значение>'
+, где
+n
+ — введенное число, а
+<получившееся значение>
+ — число, возведенное в куб.
+Проверьте работу функции с числами от
+0
+ до
+10
+. */
+
+
+
+/*
+Задание 1
+Напишите функцию для получения уникальных
+элементов массива.
+
+Для этого cоздайте функцию , которая принимает массив и возвращает новый массив с уникальными элементами.
+Используйте методы массивов для фильтрации и исключите дубликаты.
+*/
+
+//uniqueElements([ 1,2 ,3 ,3 ,4 ,4 ,1,5 ]); // [1,2,3,4,5]
+// uniqueElements([ 'apple', 'orange', 'banana', 'apple']); //['apple', 'orange', 'banana']
+
+
+
+/* const uniqueElements = (arr) => {
+    let newarr =[];
+    arr.forEach((elem) => {
+        if (!newarr.includes(elem)) {
+            newarr.push(elem);
+
+        }
+    })
+    return newarr.sort();
+}
+console.log(uniqueElements([ 'apple', 'orange', 'banana', 'apple']));
+
+ */
+
+
+
+
+/*
+Задание 2
+Напишите функцию для объединения массивов и сортировки уникальных значений.
+
+Создайте функцию , которая принимает два массива, объединяет их, удаляет дубликаты и возвращает отсортированный массив уникальных значений.
+*/
+
+// mergeAndSort([3,4,5], [1,2,6]) // [1,2,3,4,5,6]
+// mergeAndSort(['c', 'a'], [ 'b', 'a', 'd']) // ['a', 'b', 'c', 'd']
+
+
+/*
+
+const mergeAndSort = (arr,arr2) => {
+    let comboArr = arr.concat(arr2)
+    let newarr =[];
+    comboArr.forEach((elem) => {
+    if (!newarr.includes(elem)) {
+        newarr.push(elem);
+
+            }
+    })
+
+    return newarr.sort();
+
+
+}
+console.log(mergeAndSort(['c', 'a'], [ 'b', 'a', 'd']));
+
+
+ */
+
+/*
+
+Задание 3
+Напишите функцию для фильтрации чисел больше заданного значения.
+
+Создайте функцию, которая принимает массив чисел и число limit.
+Функция должна возвращать новый массив, содержащий только те числа, которые больше limit.
+*/
+
+// filterGreaterThan([1,2,3,4,10,25,125,105], 10) // [25,125,105]
+/* 
+
+const filterGreaterThan = (arr,num) => {
+    let newarr = arr.filter(number => number > num)
+    return newarr;
+}
+console.log(filterGreaterThan([1,2,3,4,10,25,125,105], 10));
+ */
